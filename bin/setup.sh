18f21367cafb91f6c0b2622debfb91618e0d8577
Jesse Zhang <sbjesse@gmail.com>
2015-03-02 15:44:33 -0800

    Installs P4V

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,5 +8,5 @@ softwareupdate --install --all
 brew install caskroom/cask/brew-cask
 brew update
 
-brew cask install iterm2 eclipse-cpp
+brew cask install iterm2 eclipse-cpp p4v
 brew install fish git
